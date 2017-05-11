1d8f4e04720e4a9d37e130886eb2ba1ab8df5b9f
Daya Adianto <dayaadianto@cs.ui.ac.id>
2017-05-12 00:42:10 +0700

    Test whether the DB file is exist (#2)

--- app-test.py
+++ app-test.py
@@ -1,4 +1,5 @@
 from app import app
+import os
 import unittest
 
 
@@ -9,6 +10,9 @@ class BasicTestCase(unittest.TestCase):
         response = tester.get('/', content_type='html/text')
 
         self.assertEqual(response.status_code, 404)
+
+    def test_database(self):
+        self.assertTrue(os.path.exists('flaskr.db'))
 
 
 if __name__ == '__main__':
