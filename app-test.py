284c65e6683b5c6d272ebf51b26e12c79bf58fd7
Daya Adianto <dayaadianto@cs.ui.ac.id>
2017-05-12 00:52:59 +0700

    Edit test so it checks a page instead of 404 (#2)

--- app-test.py
+++ app-test.py
@@ -6,12 +6,14 @@ import unittest
 class BasicTestCase(unittest.TestCase):
 
     def test_index(self):
+        '''Initial test. Ensure Flask was set up correctly.'''
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
 
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
 
     def test_database(self):
+        '''Initial test. Ensure that the database exists.'''
         self.assertTrue(os.path.exists('flaskr.db'))
 
 
