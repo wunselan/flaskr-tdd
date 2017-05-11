9f8d413ba9de0d39428ba6f4631a92bd25083e11
Daya Adianto <dayaadianto@cs.ui.ac.id>
2017-05-12 00:39:03 +0700

    Prepare app by adding imports and configs (#2)

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for,\
+                  abort, render_template, flash, jsonify
+import sqlite3
+
+# Configuration
+DATABASE = 'flaskr.db'
+DEBUG = True
+SECRET_KEY = 'my_precious'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+
+# Create and initialise app
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+app.config.from_object(__name__)
 
 
 if __name__ == '__main__':
