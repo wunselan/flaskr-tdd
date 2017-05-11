f620a3604abaa5a0abc9e2c8248660549a06421b
Daya Adianto <dayaadianto@cs.ui.ac.id>
2017-05-12 00:48:32 +0700

    Add DB initialisation codes in the app (#2)

--- app.py
+++ app.py
@@ -15,5 +15,32 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
 if __name__ == '__main__':
+    init_db()
     app.run()
