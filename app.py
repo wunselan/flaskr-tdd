2261db02ede6ca3d0c0d48eb37ae2023f9a7f426
Daya Adianto <dayaadianto@cs.ui.ac.id>
2017-05-23 00:00:03 +0700

    Add view to add new entries (#2)

--- app.py
+++ app.py
@@ -76,6 +76,21 @@ def logout():
     return redirect(url_for('index'))
 
 
+@app.route('/add', methods=['POST'])
+def add_entry():
+    '''Add new post to database.'''
+    if not session.get('logged_in'):
+        abort(401)
+
+    db = get_db()
+    db.execute(
+        'insert into entries (title, text) values (?, ?)',
+        [request.form['title'], request.form['text']]
+    )
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     init_db()
     app.run()
