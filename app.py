48271a59e569d72bce05829e3f5eeb458a8d899f
Daya Adianto <dayaadianto@cs.ui.ac.id>
2017-05-22 23:33:05 +0700

    Add new view to display entries (#2)
    The view consists of an HTML template and a handler function for
    handling requests to root ('/') path of the Web app.

--- app.py
+++ app.py
@@ -41,6 +41,15 @@ def close_db(error):
         g.sqlite_db.close()
 
 
+@app.route('/')
+def show_entries():
+    '''Searches the database for entries, then displays them.'''
+    db = get_db()
+    cur = db.execute('select * from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('index.html', entries=entries)
+
+
 if __name__ == '__main__':
     init_db()
     app.run()
