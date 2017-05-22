f658a0d0805b6b21c9dbac19b680ff695544da91
Daya Adianto <dayaadianto@cs.ui.ac.id>
2017-05-22 23:47:09 +0700

    Add view & handlers for login & logout (#2)

--- app.py
+++ app.py
@@ -50,6 +50,32 @@ def show_entries():
     return render_template('index.html', entries=entries)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    '''User login/authentication/session management.'''
+    error = None
+
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    '''User logout/authentication/session management.'''
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
     init_db()
     app.run()
