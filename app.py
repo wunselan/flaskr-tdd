bd7ed5e8c6bd8ee5e42fa31645f077960c31c116
Daya Adianto <dayaadianto@cs.ui.ac.id>
2017-05-24 13:49:56 +0700

    Reimplement view, create, and delete entries using ORM (#4)

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, session, g, redirect, url_for,\
                   abort, render_template, flash, jsonify
-import sqlite3
+from flask.ext.sqlalchemy import SQLAlchemy
+import models
+import os
+
+
+# Grabs the folder where the script runs
+basedir = os.path.abspath(os.path.dirname(__file__)
 
 # Configuration
 DATABASE = 'flaskr.db'
@@ -9,44 +15,22 @@ SECRET_KEY = 'my_precious'
 USERNAME = 'admin'
 PASSWORD = 'admin'
 
+# Defines the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# Database URL
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
 
 # Create and initialise app
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-
-def connect_db():
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
     '''Searches the database for entries, then displays them.'''
-    db = get_db()
-    cur = db.execute('select * from entries order by id desc')
-    entries = cur.fetchall()
+    entries = db.session.query(models.Flaskr)
     return render_template('index.html', entries=entries)
 
 
@@ -82,12 +66,11 @@ def add_entry():
     if not session.get('logged_in'):
         abort(401)
 
-    db = get_db()
-    db.execute(
-        'insert into entries (title, text) values (?, ?)',
-        [request.form['title'], request.form['text']]
-    )
-    db.commit()
+    new_entry = models.Flaskr(request.form['title'],
+                              request.form['text'])
+    db.session.add(new_entry)
+    db.session.commit()
+
     flash('New entry was successfully posted')
     return redirect(url_for('index'))
 
@@ -98,10 +81,11 @@ def delete_entry(post_id):
     result = { 'status': 0, 'message': 'Error' }
 
     try:
-        db = get_db()
-        db.execute('delete from entries where id=' + post_id)
-        db.commit()
+        new_id = post_id
+        db.session.query(models.Flaskr).filter_by(post_id=new_id).delete()
+        db.session.commit()
         result = { 'status': 1, 'message': 'Post Deleted' }
+        flash('The entry was deleted.')
     except Exception as e:
         result = { 'status': 0, 'message': repr(e) }
 
@@ -109,5 +93,4 @@ def delete_entry(post_id):
 
 
 if __name__ == '__main__':
-    init_db()
     app.run()
